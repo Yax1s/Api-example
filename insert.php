8eaee5bfb9ef50ad55d6e1755929a07687f7909f
yax1s <jeff.kiilu@strathmore.edu>
2022-11-19 12:46:56 +0300

    initial commit

---
+++ insert.php
@@ -0,0 +1,79 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') :
+	http_response_code(405);
+	echo json_encode([
+		'success' => 0,
+		'message' => 'Invalid Request Method. HTTP method should be POST',
+	]);
+	exit;
+endif;
+
+require 'database.php';
+$database = new database();
+$conn = $database->dbConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->name) || !isset($data->course_enrolled) || !isset($data->is_leader)) :
+
+	echo json_encode([
+		'success' => 0,
+		'message' => 'Please fill all the fields | name, course_enrolled, is_leader.',
+	]);
+	exit;
+
+elseif (empty(trim($data->name)) || empty(trim($data->course_enrolled)) || empty(trim($data->is_leader))) :
+
+	echo json_encode([
+		'success' => 0,
+		'message' => 'Oops! empty field detected. Please fill all the fields.',
+	]);
+	exit;
+
+endif;
+
+try {
+
+	$name = htmlspecialchars(trim($data->name));
+	$course_enrolled = htmlspecialchars(trim($data->course_enrolled));
+	$is_leader = htmlspecialchars(trim($data->is_leader));
+
+	$query = "INSERT INTO `students`(name,course_enrolled,is_leader) VALUES(:name,:course_enrolled,:is_leader)";
+
+	$stmt = $conn->prepare($query);
+
+	$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+	$stmt->bindValue(':course_enrolled', $course_enrolled, PDO::PARAM_STR);
+	$stmt->bindValue(':is_leader', $is_leader, PDO::PARAM_STR);
+
+	if ($stmt->execute()) {
+
+		http_response_code(201);
+		echo json_encode([
+			'success' => 1,
+			'message' => 'Data Inserted Successfully.'
+		]);
+		exit;
+	}
+
+	echo json_encode([
+		'success' => 0,
+		'message' => 'Data not Inserted.'
+	]);
+	exit;
+
+} catch (PDOException $e) {
+	http_response_code(500);
+	echo json_encode([
+		'success' => 0,
+		'message' => $e->getMessage()
+	]);
+	exit;
+}
---
+++ update.php
@@ -0,0 +1,78 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: PUT");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, is_leaderization, X-Requested-With");
+
+if ($_SERVER['REQUEST_METHOD'] !== 'PUT') :
+	http_response_code(405);
+	echo json_encode([
+		'success' => 0,
+		'message' => 'Invalid Request Method. HTTP method should be PUT',
+	]);
+	exit;
+endif;
+
+require 'database.php';
+$database = new Database();
+$conn = $database->dbConnection();
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!isset($data->id)) {
+	echo json_encode(['success' => 0, 'message' => 'Please provide the post ID.']);
+	exit;
+}
+
+try {
+
+	$fetch_post = "SELECT * FROM `students` WHERE id=:post_id";
+	$fetch_stmt = $conn->prepare($fetch_post);
+	$fetch_stmt->bindValue(':post_id', $data->id, PDO::PARAM_INT);
+	$fetch_stmt->execute();
+
+	if ($fetch_stmt->rowCount() > 0) :
+
+		$row = $fetch_stmt->fetch(PDO::FETCH_ASSOC);
+		$post_name = isset($data->name) ? $data->name : $row['name'];
+		$post_course_enrolled = isset($data->course_enrolled) ? $data->course_enrolled : $row['course_enrolled'];
+		$post_is_leader = isset($data->is_leader) ? $data->is_leader : $row['is_leader'];
+
+		$update_query = "UPDATE `students` SET name = :name, course_enrolled = :course_enrolled, is_leader = :is_leader 
+        WHERE id = :id";
+
+		$update_stmt = $conn->prepare($update_query);
+
+		$update_stmt->bindValue(':name', htmlspecialchars(strip_tags($post_name)), PDO::PARAM_STR);
+		$update_stmt->bindValue(':course_enrolled', htmlspecialchars(strip_tags($post_course_enrolled)), PDO::PARAM_STR);
+		$update_stmt->bindValue(':is_leader', htmlspecialchars(strip_tags($post_is_leader)), PDO::PARAM_INT);
+		$update_stmt->bindValue(':id', $data->id, PDO::PARAM_INT);
+
+
+		if ($update_stmt->execute()) {
+
+			echo json_encode([
+				'success' => 1,
+				'message' => 'Post updated successfully'
+			]);
+			exit;
+		}
+
+		echo json_encode([
+			'success' => 0,
+			'message' => 'Post Not updated. Something is going wrong.'
+		]);
+
+	else :
+		echo json_encode(['success' => 0, 'message' => 'Invalid ID. No students found by the ID.']);
+	endif;
+	exit;
+} catch (PDOException $e) {
+	http_response_code(500);
+	echo json_encode([
+		'success' => 0,
+		'message' => $e->getMessage()
+	]);
+	exit;
+}
